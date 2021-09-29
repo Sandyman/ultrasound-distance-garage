473cbd8d54dfc250d4607d5c054053f4cd0b6a40
Sander Huijsen <sander.huijsen@gmail.com>
2021-09-29 19:12:21 +0800

    Some refactoring.

--- main/main.ino
+++ main/main.ino
@@ -4,17 +4,16 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH    128
-#define SCREEN_HEIGHT   32
+#define OLED_SCREEN_WIDTH    128
+#define OLED_SCREEN_HEIGHT   32
 
-#define OLED_RESET      -1
-#define SCREEN_ADDRESS  0x3C
+#define OLED_RESET           -1
+#define OLED_SCREEN_ADDRESS  0x3C
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+Adafruit_SSD1306 display(OLED_SCREEN_WIDTH, OLED_SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-const int pixelPin = 12;
-const int trigPin = 25; // Trigger Pin of Ultrasonic Sensor
-const int echoPin = 26; // Echo Pin of Ultrasonic Sensor
+const int TrigPin = 25; // Trigger Pin of Ultrasonic Sensor
+const int EchoPin = 26; // Echo Pin of Ultrasonic Sensor
 
 bool logMessage = false;
 
@@ -29,37 +28,39 @@ int buffer_p = 0;
 
 #define N_PIXELS  13
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_PIXELS, pixelPin, NEO_GRB + NEO_KHZ800);
+const int PixelPin = 12;
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_PIXELS, PixelPin, NEO_GRB + NEO_KHZ800);
 
 #define FOREVER         for (;;)
 
 #define O   strip.Color(0, 0, 0)
-#define R   strip.Color(80, 0, 0)
-#define G   strip.Color(0, 80, 0)
+#define R   strip.Color(255, 0, 0)
+#define G   strip.Color(0, 255, 0)
 
 uint32_t bars[15][N_PIXELS] = {
-  {O, O, O, O, O, O, O, O, O, O, O, O, O}, // 0
-  {G, O, O, O, O, O, O, O, O, O, O, O, G}, // 1
-  {G, G, O, O, O, O, O, O, O, O, O, G, G}, // 2
-  {G, G, G, O, O, O, O, O, O, O, G, G, G}, // 3
-  {G, G, G, G, O, O, O, O, O, G, G, G, G}, // 4
-  {G, G, G, G, G, O, O, O, G, G, G, G, G}, // 5
-  {G, G, G, G, G, G, O, G, G, G, G, G, G}, // 6
-  {G, G, G, G, G, G, G, G, G, G, G, G, G}, // 7
-  {G, G, G, G, G, G, R, G, G, G, G, G, G}, // 8
-  {G, G, G, G, G, R, R, R, G, G, G, G, G}, // 9
+  {O, O, O, O, O, O, O, O, O, O, O, O, O}, //  0 - too far
+  {G, O, O, O, O, O, O, O, O, O, O, O, G}, //  1
+  {G, G, O, O, O, O, O, O, O, O, O, G, G}, //  2
+  {G, G, G, O, O, O, O, O, O, O, G, G, G}, //  3
+  {G, G, G, G, O, O, O, O, O, G, G, G, G}, //  4
+  {G, G, G, G, G, O, O, O, G, G, G, G, G}, //  5
+  {G, G, G, G, G, G, O, G, G, G, G, G, G}, //  6
+  {G, G, G, G, G, G, G, G, G, G, G, G, G}, //  7
+  {G, G, G, G, G, G, R, G, G, G, G, G, G}, //  8
+  {G, G, G, G, G, R, R, R, G, G, G, G, G}, //  9
   {G, G, G, G, R, R, R, R, R, G, G, G, G}, // 10
   {G, G, G, R, R, R, R, R, R, R, G, G, G}, // 11
   {G, G, R, R, R, R, R, R, R, R, R, G, G}, // 12
   {G, R, R, R, R, R, R, R, R, R, R, R, G}, // 13
-  {R, R, R, R, R, R, R, R, R, R, R, R, R}, // 14
+  {R, R, R, R, R, R, R, R, R, R, R, R, R}, // 14 - too close!
 };
 
 void setup() {
   Serial.begin(115200); // Starting Serial Terminal
   Serial.println(F("Power on!"));
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed! STOP."));
     FOREVER;
   }
@@ -68,31 +69,38 @@ void setup() {
   display.display();
 
   strip.begin();
-  strip.setBrightness(50);
+  strip.setBrightness(10);
   strip.show();
 
-  pinMode(trigPin, OUTPUT);
-  digitalWrite(trigPin, LOW);
-  pinMode(echoPin, INPUT);
+  pinMode(TrigPin, OUTPUT);
+  digitalWrite(TrigPin, LOW);
+  pinMode(EchoPin, INPUT);
+
+  FOREVER {
+    long duration, distance;
+
+    duration = get_filtered_duration();
+    distance = add_and_get_average(microsecondsToCentimeters(duration));
+    show_distance(distance);
+
+    delay(20);
+  }
 }
 
-void loop() {
-  long duration, distance;
+// Not used - preparing for deep sleep
+void loop() {}
 
-  duration = get_filtered_duration();
-
-  distance = add_and_get_average(microsecondsToCentimeters(duration));
-
-  draw_distance(distance);
-
-  delay(50);
+void ultrasound_trigger() {
+   digitalWrite(TrigPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(TrigPin, LOW);
 }
 
 long get_filtered_duration() {
   // Store multiple measurements
   for (uint8_t i = 0; i < filter_len; i++) {
-    trigger();
-    filterArray[i] = pulseIn(echoPin, HIGH);
+    ultrasound_trigger();
+    filterArray[i] = pulseIn(EchoPin, HIGH);
     delay(20);
   }
 
@@ -115,19 +123,6 @@ long get_filtered_duration() {
   return sum / 3;
 }
 
-void bar(uint8_t bar_num) {
-  for (uint16_t i = 0; i < N_PIXELS; i++) {
-    strip.setPixelColor(i, bars[bar_num][i]);
-  }
-  strip.show();
-}
-
-void trigger() {
-   digitalWrite(trigPin, HIGH);
-   delayMicroseconds(10);
-   digitalWrite(trigPin, LOW);
-}
-
 long microsecondsToCentimeters(long microseconds) {
   return microseconds / 29 / 2;
 }
@@ -145,34 +140,49 @@ int add_and_get_average(const int cm) {
   return avg / buffer_len;
 }
 
-void draw_distance(const int cm) {
-  int previous_cm = 0;
-
+void display_setup() {
   display.clearDisplay();
 
   display.setTextColor(SSD1306_WHITE);
   display.setTextSize(4);
+}
 
-  if (cm > 150) {
-    display.setCursor(8,4);
-    sprintf(sbuf, "%3s", "SLOW");
-    display.print(F(sbuf));
-  } else if (cm < 11) {
-    display.setCursor(8,4);
-    display.print(F("STOP!"));
-  } else {
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(16,4); // (12,11)
-    sprintf(sbuf, "%3d", cm);
-    display.print(F(sbuf));
-  
-    display.setTextSize(2);
-    display.setCursor(100,4);
-    display.print(F("cm"));
-  }
+void display_slow() {
+  display.setCursor(8,4);
+  sprintf(sbuf, "%3s", "SLOW");
+  display.print(F(sbuf));
 
   display.display();
+}
 
+void display_stop() {
+  display.setCursor(8,4);
+  display.print(F("STOP!"));
+
+  display.display();
+}
+
+void display_distance(const int cm) {
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(16,4);
+  sprintf(sbuf, "%3d", cm);
+  display.print(F(sbuf));
+
+  display.setTextSize(2);
+  display.setCursor(100,4);
+  display.print(F("cm"));
+
+  display.display();
+}
+
+void draw_bar(uint8_t bar_num) {
+  for (uint16_t i = 0; i < N_PIXELS; i++) {
+    strip.setPixelColor(i, bars[bar_num][i]);
+  }
+  strip.show();
+}
+
+void display_bar(const int cm) {
   // Calculate the index into the bar table...
   int8_t bar_ptr = (-3 * cm + 175) / 10;
   if (bar_ptr < 0) {
@@ -180,8 +190,24 @@ void draw_distance(const int cm) {
   } else if (bar_ptr > 14) {
     bar_ptr = 14;
   }
-  // .. and show the bar
-  bar(bar_ptr);
+
+  // .. and draw the bar
+  draw_bar(bar_ptr);
+}
+
+void show_distance(const int cm) {
+  static int previous_cm = 0;
+
+  display_setup();
+  if (cm > 150) {
+    display_slow();
+  } else if (cm < 11) {
+    display_stop();
+  } else {
+    display_distance(cm);
+  }
+
+  display_bar(cm);
 
   if (previous_cm != cm) {
     if (cm <= 56) {
